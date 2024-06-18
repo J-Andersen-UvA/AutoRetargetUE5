c8cc3fca9059f613bd7a647b48d4134d2adfc262
J-Andersen-UvA <j.andersen@uva.nl>
2024-06-18 14:00:16 +0200

    there is no need to do any ikrig chain creation by hand. Since ue5_4 we can use an automated process to create the chains and ge tthe root based on known templates. We are using this for our implementation now because it is more scalable.

--- ikRigCreator.py
+++ ikRigCreator.py
@@ -15,36 +15,36 @@ def createIKRig(meshName : str = "glassesGuyNew") -> bool:
         Args:
             meshName (str): The name of the mesh to create the IK rig for
     '''
-    def setBoneChains(ik_rig_controller):
-        # Add retarget chain
-        # SPINE AND HEAD
-        ik_rig_controller.add_retarget_chain("Spine", "Spine", "Spine2", unreal.Name())
-        ik_rig_controller.add_retarget_chain("Neck", "Neck", "Neck", unreal.Name())
-        ik_rig_controller.add_retarget_chain("Head", "Head", "Head", unreal.Name())
+    # def setBoneChains(ik_rig_controller):
+    #     # Add retarget chain
+    #     # SPINE AND HEAD
+    #     ik_rig_controller.add_retarget_chain("Spine", "Spine", "Spine2", unreal.Name())
+    #     ik_rig_controller.add_retarget_chain("Neck", "Neck", "Neck", unreal.Name())
+    #     ik_rig_controller.add_retarget_chain("Head", "Head", "Head", unreal.Name())
 
-        # LEGS
-        ik_rig_controller.add_retarget_chain("LeftLeg", "LeftUpLeg", "LeftToeBase", "LeftFootIK")
-        ik_rig_controller.add_retarget_chain("RightLeg", "RightUpLeg", "RightToeBase", "RightFootIK")
+    #     # LEGS
+    #     ik_rig_controller.add_retarget_chain("LeftLeg", "LeftUpLeg", "LeftToeBase", "LeftFootIK")
+    #     ik_rig_controller.add_retarget_chain("RightLeg", "RightUpLeg", "RightToeBase", "RightFootIK")
 
-        # ARMS
-        ik_rig_controller.add_retarget_chain("LeftClavicle", "LeftShoulder", "LeftShoulder", unreal.Name())
-        ik_rig_controller.add_retarget_chain("RightClavicle", "RightShoulder", "RightShoulder", unreal.Name())
-        ik_rig_controller.add_retarget_chain("LeftArm", "LeftArm", "LeftHand", "LeftHandIK")
-        ik_rig_controller.add_retarget_chain("RightArm", "RightArm", "RightHand", "RightHandIK")
+    #     # ARMS
+    #     ik_rig_controller.add_retarget_chain("LeftClavicle", "LeftShoulder", "LeftShoulder", unreal.Name())
+    #     ik_rig_controller.add_retarget_chain("RightClavicle", "RightShoulder", "RightShoulder", unreal.Name())
+    #     ik_rig_controller.add_retarget_chain("LeftArm", "LeftArm", "LeftHand", "LeftHandIK")
+    #     ik_rig_controller.add_retarget_chain("RightArm", "RightArm", "RightHand", "RightHandIK")
 
-        # HANDS
-        ik_rig_controller.add_retarget_chain("LeftThumb", "LeftHandThumb1", "LeftHandThumb3", unreal.Name())
-        ik_rig_controller.add_retarget_chain("LeftIndex", "LeftHandIndex1", "LeftHandIndex3", unreal.Name())
-        ik_rig_controller.add_retarget_chain("LeftMiddle", "LeftHandMiddle1", "LeftHandMiddle3", unreal.Name())
-        ik_rig_controller.add_retarget_chain("LeftRing", "LeftHandRing1", "LeftHandRing3", unreal.Name())
-        ik_rig_controller.add_retarget_chain("LeftPinky", "LeftHandPinky1", "LeftHandPinky3", unreal.Name())
-        ik_rig_controller.add_retarget_chain("RightThumb", "RightHandThumb1", "RightHandThumb3", unreal.Name())
-        ik_rig_controller.add_retarget_chain("RightIndex", "RightHandIndex1", "RightHandIndex3", unreal.Name())
-        ik_rig_controller.add_retarget_chain("RightMiddle", "RightHandMiddle1", "RightHandMiddle3", unreal.Name())
-        ik_rig_controller.add_retarget_chain("RightRing", "RightHandRing1", "RightHandRing3", unreal.Name())
-        ik_rig_controller.add_retarget_chain("RightPinky", "RightHandPinky1", "RightHandPinky3", unreal.Name())
+    #     # HANDS
+    #     ik_rig_controller.add_retarget_chain("LeftThumb", "LeftHandThumb1", "LeftHandThumb3", unreal.Name())
+    #     ik_rig_controller.add_retarget_chain("LeftIndex", "LeftHandIndex1", "LeftHandIndex3", unreal.Name())
+    #     ik_rig_controller.add_retarget_chain("LeftMiddle", "LeftHandMiddle1", "LeftHandMiddle3", unreal.Name())
+    #     ik_rig_controller.add_retarget_chain("LeftRing", "LeftHandRing1", "LeftHandRing3", unreal.Name())
+    #     ik_rig_controller.add_retarget_chain("LeftPinky", "LeftHandPinky1", "LeftHandPinky3", unreal.Name())
+    #     ik_rig_controller.add_retarget_chain("RightThumb", "RightHandThumb1", "RightHandThumb3", unreal.Name())
+    #     ik_rig_controller.add_retarget_chain("RightIndex", "RightHandIndex1", "RightHandIndex3", unreal.Name())
+    #     ik_rig_controller.add_retarget_chain("RightMiddle", "RightHandMiddle1", "RightHandMiddle3", unreal.Name())
+    #     ik_rig_controller.add_retarget_chain("RightRing", "RightHandRing1", "RightHandRing3", unreal.Name())
+    #     ik_rig_controller.add_retarget_chain("RightPinky", "RightHandPinky1", "RightHandPinky3", unreal.Name())
 
-        return ik_rig_controller
+    #     return ik_rig_controller
 
     # Define the save path
     save_path = f"/Game/IKRigs/{meshName}IKRig.{meshName}IKRig"
@@ -68,16 +68,18 @@ def createIKRig(meshName : str = "glassesGuyNew") -> bool:
     skeletal_mesh = unreal.EditorAssetLibrary.load_asset(mesh_path)
     ik_rig_controller.set_skeletal_mesh(skeletal_mesh)
 
-    # Create IK Goals
-    ik_rig_controller.add_new_goal("LeftFootIK", "LeftToeBase")
-    ik_rig_controller.add_new_goal("RightFootIK", "RightToeBase")
-    ik_rig_controller.add_new_goal("LeftHandIK", "LeftHand")
-    ik_rig_controller.add_new_goal("RightHandIK", "RightHand")
+    # # Create IK Goals
+    # ik_rig_controller.add_new_goal("LeftFootIK", "LeftToeBase")
+    # ik_rig_controller.add_new_goal("RightFootIK", "RightToeBase")
+    # ik_rig_controller.add_new_goal("LeftHandIK", "LeftHand")
+    # ik_rig_controller.add_new_goal("RightHandIK", "RightHand")
 
-    # Set retargetRootBone
-    ik_rig_controller.set_retarget_root("Hips")
+    # # Set retargetRootBone
+    # ik_rig_controller.set_retarget_root("Hips")
 
-    ik_rig_controller = setBoneChains(ik_rig_controller)
+    # ik_rig_controller = setBoneChains(ik_rig_controller)
+
+    ik_rig_controller.apply_auto_generated_retarget_definition()
 
     # Use apply_auto_fbik to attempt to automatically generate the IK rig
     if (not ik_rig_controller.apply_auto_fbik()):
