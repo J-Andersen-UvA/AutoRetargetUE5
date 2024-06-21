5e1605ce13cb8ae439f1ce31b74ac92600483673
J-Andersen-UvA <j.andersen@uva.nl>
2024-06-21 15:18:23 +0200

    Retargeting animations now possible through retarget_animation

--- IKRetargeter.py
+++ IKRetargeter.py
@@ -13,8 +13,6 @@ def create_retargeter(source_rig_path : str, target_rig_path : str, rtg_name : s
     rtg_controller = unreal.IKRetargeterController.get_controller(rtg)
     rtg_controller.set_ik_rig(unreal.RetargetSourceOrTarget.SOURCE, source_rig)
     rtg_controller.set_ik_rig(unreal.RetargetSourceOrTarget.TARGET, target_rig)
-    # rtg_controller.set_preview_mesh(unreal.RetargetSourceOrTarget.SOURCE, source_skel_mesh)
-    # rtg_controller.set_preview_mesh(unreal.RetargetSourceOrTarget.TARGET, target_skel_mesh)
 
     # Clear mapping then auto-map chains
     rtg_controller.auto_map_chains(unreal.AutoMapChainType.CLEAR, True)
@@ -23,10 +21,33 @@ def create_retargeter(source_rig_path : str, target_rig_path : str, rtg_name : s
     # Auto align bones
     rtg_controller.auto_align_all_bones(unreal.RetargetSourceOrTarget.SOURCE)
 
+# Helper function to convert AnimSequence to AssetData
+def get_asset_data(animation):
+    return unreal.AssetRegistryHelpers.get_asset_registry().get_asset_by_object_path(animation.get_path_name())
+
+# Retarget an animation using the given retargeter
+def retarget_animation(animation_path: str, retargeter_path: str) -> bool:
+    # Load the animation
+    animation_paths = animation_path.split(',')
+    animations = [unreal.EditorAssetLibrary.load_asset(path) for path in animation_paths]
+    print(animations)
+    retargeter = unreal.EditorAssetLibrary.load_asset(retargeter_path)
+    source_mesh = unreal.IKRetargeterController.get_controller(retargeter).get_preview_mesh(unreal.RetargetSourceOrTarget.SOURCE)
+    target_mesh = unreal.IKRetargeterController.get_controller(retargeter).get_preview_mesh(unreal.RetargetSourceOrTarget.TARGET)
+
+    animations_asset_data = [get_asset_data(animation) for animation in animations]
+    unreal.IKRetargetBatchOperation.duplicate_and_retarget(animations_asset_data, source_mesh, target_mesh, retargeter, suffix='_retargeted_to_' + target_mesh.get_name())
+
 # Example usage
+# print("++++++++++++++")
 # Load the source and target rigs
-# source_rig = unreal.EditorAssetLibrary.load_asset('/Game/IKRigs/glassesGuyNewIKRig')
-# target_rig = unreal.EditorAssetLibrary.load_asset('/Game/IKRigs/NemuIKRig')
+# source_rig_path = '/Game/IKRigs/glassesGuyNewIKRig'
+# target_rig_path = '/Game/IKRigs/NemuIKRig'
 
 # # Call the create_retargeter function
-# create_retargeter(source_rig, target_rig, rtg_name='glassesGuyToNemuRTG')
+# create_retargeter(source_rig_path, target_rig_path, rtg_name='glassesGuyToNemuRTG')
+
+# Retarget an animation using the retargeter
+# anim_path = "/Game/Anims/APPEL_Anim"
+# retargeter_path = "/Game/Retargets/glassesGuyToNemuRTG"
+# retarget_animation(anim_path, retargeter_path)
