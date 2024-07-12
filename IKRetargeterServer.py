cf578c84385e2a31da422beba2188dae2d9d8ded
J-Andersen-UvA <j.andersen@uva.nl>
2024-07-12 17:03:59 +0200

    removed some logs.

--- IKRetargeterServer.py
+++ IKRetargeterServer.py
@@ -340,6 +340,7 @@ class Retargeter:
             self.send_response(self.current_connection, "Invalid URL format, unable to extract file name.")
             raise ValueError("Invalid URL format, unable to extract file name.")
         if os.path.exists(destination_path + file_name):
+            print(f"File already exists at path({destination_path + file_name}).")
             self.send_response(self.current_connection, f"File already exists at path({destination_path + file_name}).")
             return
 
@@ -567,9 +568,7 @@ class Retargeter:
         pass
 
     def fetch_server_type(self):
-        print("Fetching server type...")
         server_type = getattr(self.thread_local, 'serverType', None)
-        print("Server type:", server_type)
         return server_type
 
 
