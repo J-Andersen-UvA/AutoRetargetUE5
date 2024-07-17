55cead229619e34897f207b1e020a09f4dac834c
J-Andersen-UvA <j.andersen@uva.nl>
2024-07-17 13:28:41 +0200

    fixed import

--- rigRetargetHelpers/IKRetargeter.py
+++ rigRetargetHelpers/IKRetargeter.py
@@ -1,5 +1,5 @@
 import unreal
-import fetchUEInfo
+from ..helpers import fetchUEInfo
 
 # Using 2 IK-rigs, create a retargeter between them
 def create_retargeter(source_rig_path : str, target_rig_path : str, rtg_name : str="RTG") -> bool:
